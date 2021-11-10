e44b32da8c0e575d5d5ce35f0c2e289673564bdb
TwoBee <mail@tobiaszimmer.dev>
2021-11-10 13:57:32 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Sample Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 };
