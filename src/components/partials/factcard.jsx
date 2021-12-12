ea9e03f88c5051011a8806697baf5d58417b4e56
TwoBee <mail@tobiaszimmer.dev>
2021-12-12 17:11:09 +0100

    mobile fix4factcards

--- src/components/partials/factcard.jsx
+++ src/components/partials/factcard.jsx
@@ -10,9 +10,9 @@ const Factcard = ({ headline, subline, text }) => {
           animate-tilt">
       </div>
       <div class="relative w-8/12 h-full bg-gradient-to-br from-teal-400 to-indigo-400  opacity-75 rounded-lg shadow p-2 mx-auto">
-        <h1 class="text-xl text-white font-bold m-4 mb-0 opacity-0 group-hover:opacity-100 group-hover:duration-300 transition duration-500">{headline}</h1>
-        <h2 class="text-lg text-white font-bold m-4 opacity-0 group-hover:opacity-100 group-hover:duration-300 transition duration-700">{subline}</h2>
-        <p class="text-white m-4 opacity-0 group-hover:opacity-100 group-hover:duration-300 transition duration-1000">{text}</p>
+        <h1 class="text-xl text-white font-bold m-4 mb-0 md:opacity-0 group-hover:opacity-100 group-hover:duration-300 transition duration-500">{headline}</h1>
+        <h2 class="text-lg text-white font-bold m-4 md:opacity-0 group-hover:opacity-100 group-hover:duration-300 transition duration-700">{subline}</h2>
+        <p class="text-white m-4 md:opacity-0 group-hover:opacity-100 group-hover:duration-300 transition duration-1000">{text}</p>
       </div>
     </div>
   )
